b87391ca61efde2e77457a720e8082b6f08ded85
Anton_Pikhtin <anton_pikhtin@epam.com>
2019-06-27 17:08:02 +0300

    Initial commit. Created simple spring boot application with web, jpa and h2 dependencies.

---
+++ src/main/java/com/epam/rgntest/RgntestApplication.java
@@ -0,0 +1,13 @@
+package com.epam.rgntest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RgntestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RgntestApplication.class, args);
+	}
+
+}
